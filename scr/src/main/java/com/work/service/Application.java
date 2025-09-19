983b8b2fb0ddd016aa85b6ce82cc515e94be0ee8
ysss <xnmym_2020@qq.com>
2025-09-19 13:31:16 +0800

    启动程序

---
+++ scr/src/main/java/com/work/service/Application.java
@@ -0,0 +1,14 @@
+package com.work.service;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.work.service.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
