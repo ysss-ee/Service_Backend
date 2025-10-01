f7e4b3075cf846eec5e1775909c1b51ae2448a0a
Reaper <2627887384@qq.com>
2025-10-01 23:29:05 +0800

    接口1、3 jwt修改 1.0

---
+++ Project/src/main/java/com/work/service/dto/request/DeletePostRequest.java
@@ -0,0 +1,9 @@
+package com.work.service.dto.request;
+
+import lombok.Data;
+
+@Data
+public class DeletePostRequest {
+    //private Integer userId;
+    private Integer postId;
+}
---
+++ Project/src/main/java/com/work/service/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.work.service.mapper;
+
+import com.work.service.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Reaper
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2025-09-17 13:21:02
+* @Entity com.example.demo.entity.Post
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
