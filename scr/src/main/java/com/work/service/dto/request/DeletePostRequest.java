179a746fb7c25f2f203d924ed9a71104867af4cd
ysss-ee <xnmym_2020@qq.com>
2025-10-02 15:16:15 +0800

    Delete scr directory

--- scr/src/main/java/com/work/service/dto/request/DeletePostRequest.java
+++
@@ -1,9 +0,0 @@
-package com.work.service.dto.request;
-
-import lombok.Data;
-
-@Data
-public class DeletePostRequest {
-    //private Integer userId;
-    private Integer postId;
-}
--- scr/src/main/java/com/work/service/dto/request/ManageRequest.java
+++
@@ -1,9 +0,0 @@
-package com.work.service.dto.request;
-
-import lombok.Data;
-
-@Data
-public class ManageRequest {
-    //private Integer userId;
-    private Integer userType;
-}
